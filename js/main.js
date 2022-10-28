f1bcf34bbb776a7dadc86161314168c624a5fa20
sam raphael <samraphael@gmail.com>
2022-10-28 14:36:02 -0400

    ide formatting

--- js/main.js
+++ js/main.js
@@ -34,20 +34,20 @@ function hidePageComponents() {
 /** Overall function to kick off the app. */
 
 async function start() {
-  console.debug("start");
+    console.debug("start");
 
-  // "Remember logged-in user" and log in, if credentials in localStorage
-  await checkForRememberedUser();
-  await getAndShowStoriesOnStart();
+    // "Remember logged-in user" and log in, if credentials in localStorage
+    await checkForRememberedUser();
+    await getAndShowStoriesOnStart();
 
-  // if we got a logged-in user
-  if (currentUser) updateUIOnUserLogin();
+    // if we got a logged-in user
+    if (currentUser) updateUIOnUserLogin();
 }
 
 // Once the DOM is entirely loaded, begin the app
 
 console.warn("HEY STUDENT: This program sends many debug messages to" +
-  " the console. If you don't see the message 'start' below this, you're not" +
-  " seeing those helpful debug messages. In your browser console, click on" +
-  " menu 'Default Levels' and add Verbose");
+    " the console. If you don't see the message 'start' below this, you're not" +
+    " seeing those helpful debug messages. In your browser console, click on" +
+    " menu 'Default Levels' and add Verbose");
 $(start);
